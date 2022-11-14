4574fd172076418e2610ae434402c9b75d76507b
brennanp93 <brennan.p.perez@gmail.com>
2022-11-14 14:51:13 -0800

    create list of finches. render about and index pages

--- main_app/views.py
+++ main_app/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
 
+
+finches = [
+    {'name': 'House Finch', 'migratory': 'False', 'range': '' },
+    {'name': 'Lesser Goldfinch', 'migratory': 'True', 'range': 'All throughout CA' },
+    {'name': 'American Goldfinch', 'migratory': 'True', 'range': 'Canada to Southern US' },
+    {'name': 'Purple Finch', 'migratory': 'True', 'range': 'All throughout US and Canada' }
+]
+
 # Create your views here.
+
+def home(request):
+    return render(request, 'home.html')
+
+def about(request):
+    return render(request, 'about.html')
+
+def finches_index(request):
+    return render(request, 'finches/index.html', { 'finches': finches
+    })
